d5a3e522fb551dc76b3111e3f039d4bf49ce07c4
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2019-01-09 20:58:32 +0100

    Aufgabe 2

---
+++ src/Array.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.util.Random;
+
+public class Array
+{
+public static void main (String []args) {
+    int x = Integer.parseInt(JOptionPane.showInputDialog("Wie groß soll das Array sein?")); //Größe eingeben, string wird in Int umgewandelt.
+    int[] array1 = new int[x];
+
+    for (int i = 0; i < x; i++) {
+        Random zahl = new Random();
+        array1[i] = zahl.nextInt(1000);
+    }
+
+    for(int i=array1.length -1; i >=0;i--)
+    {
+        System.out.println(array1[i]);
+    }
+}
+}
